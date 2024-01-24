0891e6d3619c2439756ae2b1034cead2750f891a
ukfIG2 <ivan.gabris2@student.ukf.sk>
2024-01-24 15:37:08 +0100

    lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DomovViewVue from '@/views/DomovView.vue'
-import ProduktView from '@/views/ProduktView.vue'
-import KontaktView from '@/views/KontaktView.vue'
-import LoginView from '@/views/LoginView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: DomovViewVue },
-    { path: '/products', component: ProduktView },
-    { path: '/contact', component: KontaktView },
-    { path: '/Login', component: LoginView },
+    { path: '/', component: () => import('@/views/DomovView.vue') },
+    { path: '/products', component: () => import('@/views/ProduktView.vue') },
+    { path: '/contact', component: () => import('@/views/KontaktView.vue') },
+    { path: '/Login', component: () => import('@/views/LoginView.vue') },
   ]
 });
+
 export default router
